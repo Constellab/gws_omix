3bae05fed3f5ab66e0801203d9c36081db107120
rdoliveira <rdeoliveira@gencovery.com>
2021-11-04 12:04:20 +0000

    (fix) salmon mapping class correction

--- src/gws_omix/rna_seq/salmon_quant_mapping.py
+++ src/gws_omix/rna_seq/salmon_quant_mapping.py
@@ -7,7 +7,7 @@ import os
 import re
 import csv
 
-from gws_core import task_decorator, File, IntParam, StrParam, ConfigParams, TaskInputs, TaskOutputs, Utils
+from gws_core import task_decorator, File, IntParam, StrParam, ConfigParams, TaskInputs, TaskOutputs, Utils, Folder
 from ..base_env.omix_env_task import BaseOmixEnvTask
 from ..file.fasta_file import FastaFile
 from ..file.fastq_file import FastqFile
@@ -24,13 +24,13 @@ class SalmonQuantMapping(BaseOmixEnvTask):
     input_specs = {
         'fastq_files_forward': (FastqFile,),
         'fastq_files_reverse': (FastqFile,),
-        'salmon_genome_index': (File,)       
+        'salmon_genome_index': (Folder,)       
     }
     output_specs = { 
         'Salmon_quant_file': (File,) 
     }
     config_specs = {
-        "threads": IntParam(default_value=12, min_value=1, short_description="Number of threads [Default =  12] ")
+        "threads": IntParam(default_value=2, min_value=1, short_description="Number of threads [Default =  2] ")
      }
    
     def gather_outputs(self, params: ConfigParams, inputs: TaskInputs) -> TaskOutputs:
@@ -40,9 +40,9 @@ class SalmonQuantMapping(BaseOmixEnvTask):
     
     def build_command(self, params: ConfigParams, inputs: TaskInputs) -> list:
         thread = params["threads"]
-        reads_forward  = params["fastq_files_forward"]
-        reads_reverse = params["fastq_files_reverse"]
-        transcripts_index = params["salmon_genome_index"]  
+        reads_forward  = inputs["fastq_files_forward"]
+        reads_reverse = inputs["fastq_files_reverse"]
+        transcripts_index = inputs["salmon_genome_index"]  
 
         cmd = [
             "salmon quant -i ",transcripts_index,
