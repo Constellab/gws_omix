954ca6ecea637e9ebb9b3a708d92f9636ac1398e
rdolvieira <rdeoliveira@gencovery.com>
2022-08-22 08:18:20 +0000

    (add) hook scripts to post-install tools

---
+++ .hooks/post-install.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+# LICENSE
+# This software is the exclusive property of Gencovery SAS. 
+# The use and distribution of this software is prohibited without the prior consent of Gencovery SAS.
+# About us: https://gencovery.com
+
+# Post-installation script executed after module installation
+
+apt-get -y install pigz
