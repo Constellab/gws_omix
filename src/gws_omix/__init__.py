0963e292b7a145688e941b6f90e7b84c82cbfb02
rdoliveira <rdeoliveira@gencovery.com>
2023-06-14 08:28:05 +0000

    (fix) unit test for omix for existing task

--- src/gws_omix/__init__.py
+++ src/gws_omix/__init__.py
@@ -6,11 +6,6 @@ from .aligment.deepec import DeepEC
 from .aligment.deepec_ec_list_extractor import DeepECListExtractor
 # Merge EC List
 from .aligment.ec_list_merger import ECListMerger
-# Gmap alignment
-# from .aligment.gmap_align_to_gff3 import GmapAlignGFF3
-from .aligment.gmap_align_to_gtf import GmapAlignGTF
-# Gmap indexing
-from .aligment.gmap_index import GmapIndex
 # data filtering
 from .data_filtering.trim_galore import TrimGalore
 from .file.blast_ec_file import BlastECFile
@@ -23,6 +18,5 @@ from .file.gtf_file import GTFFile
 from .rna_seq.deseq2_differential_expression import DESeq2DifferentialAnalysis
 from .rna_seq.htseq_count import HTSeqCount
 from .rna_seq.salmon_index import SalmonIndex
-# from .aligment.gmap_align_to_fasta import GmapAlignFasta
 # Merge EC List
 from .rna_seq.salmon_quant_mapping import SalmonQuantMapping
