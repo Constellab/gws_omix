0d3238aa7e3296703db53685f32e98cb5f1824ef
Nour <nlarifi@gencovery.com>
2024-01-18 15:33:06 +0000

    pydesq2_task_implementation

---
+++ src/gws_omix/rna_seq/_pydesq2_count.py
@@ -0,0 +1,131 @@
+from pydeseq2.dds import DeseqDataSet
+from pydeseq2.ds import DeseqStats
+import pandas as pd
+import scanpy as sc
+import numpy as np
+import seaborn as sns
+import decoupler as dc
+import matplotlib.pyplot as plt
+import sys
+
+class DeseqAnalyzer:
+    def __init__(self, count_table_path, metadata_path, genes_colname, control_condition=None, unnormal_condition=None, padj_value=None, log2FoldChange_value=None):
+        self.count_table_path = count_table_path
+        self.metadata_path = metadata_path
+        self.genes_colname = genes_colname
+        self.control_condition = control_condition
+        self.unnormal_condition = unnormal_condition
+        self.padj_value = padj_value
+        self.log2FoldChange_value = log2FoldChange_value
+        self.sigs = None
+        self.dds = None
+
+    def load_data(self):
+        # Load count matrix and metadata
+        counts = pd.read_csv(self.count_table_path)
+        metadata = pd.read_csv(self.metadata_path, sep='\t', comment='#')
+
+        # Set geneid as index
+        counts = counts.set_index(self.genes_colname)
+
+        # Remove rows with zero counts
+        counts = counts[counts.sum(axis=1) > 0]
+
+        # Transpose the matrix
+        counts = counts.T
+
+        # Set the sample column as index
+        metadata = metadata.set_index('Sample')
+
+        # Create DeseqDataSet
+        self.dds = DeseqDataSet(counts=counts, metadata=metadata, design_factors="Condition")
+
+    def run_deseq_stats(self):
+        # Run Deseq2
+        self.dds.deseq2()
+
+        # Perform Wald test
+        stat_res = DeseqStats(self.dds, contrast=('Condition', self.unnormal_condition, self.control_condition))
+        stat_res.summary()
+        res = stat_res.results_df
+
+        # Filter significant genes
+        self.sigs = res[(res.padj < self.padj_value) & (abs(res.log2FoldChange) > self.log2FoldChange_value)]
+
+    def save_desq2_results_table(self):
+        # Save significant genes to CSV
+        self.sigs.to_csv('pydesq2_results_table.csv', index=True)
+
+    def perform_pca(self):
+        # Normalization for PCA
+        self.dds.layers['counts'] = self.dds.X.copy()
+        sc.pp.normalize_total(self.dds)
+        sc.tl.pca(self.dds)
+
+    def save_pca_metadata(self):
+        # Access the PCA coordinates
+        pca_coords = pd.DataFrame(self.dds.obsm['X_pca'][:, :2], columns=['PC1', 'PC2'])
+        metadata = pd.read_csv(self.metadata_path, sep='\t', comment='#')
+        pca_metadata = pd.concat([pca_coords, metadata], axis=1, join='inner')
+
+        # Save PCA metadata
+        pca_metadata.to_csv('pca_metadata.csv', index=False)
+
+    def save_pca_proportions(self):
+        # Access PCA proportions
+        pca_proportions = self.dds.uns['pca']['variance_ratio'][:2]
+        pca_proportions_df = pd.DataFrame({'PC1 Proportion': [pca_proportions[0]], 'PC2 Proportion': [pca_proportions[1]]})
+
+        # Save PCA proportions
+        pca_proportions_df.to_csv('pca_proportions.csv', index=False)
+
+    def generate_clustermap(self):
+        # Normalized counts using log1p for the heatmap
+        self.dds.layers['log1p'] = np.log1p(self.dds.layers['normed_counts'])
+
+        # Select significant genes for heatmap
+        dds_sigs = self.dds[:, self.sigs.index]
+        grapher = pd.DataFrame(dds_sigs.layers['log1p'].T, index=dds_sigs.var_names, columns=dds_sigs.obs_names)
+
+        # Create a seaborn clustermap
+        clustermap = sns.clustermap(grapher, z_score=0, cmap='RdYlBu_r')
+
+        # Save the clustermap as a PNG file
+        clustermap.savefig('Heatmap.png')
+
+    def generate_volcano_plot(self):
+        # Assuming 'res' is the result dataframe
+        dc.plot_volcano_df(self.sigs, x='log2FoldChange', y='padj', top=30)
+
+        # Save the figure to a PNG file
+        plt.savefig('volcano_plot.png')
+
+    def run_analysis(self):
+        self.load_data()
+        self.run_deseq_stats()
+        self.perform_pca()
+        self.save_pca_metadata()
+        self.save_pca_proportions()
+        self.save_desq2_results_table()  # Save significant genes to CSV
+        self.generate_clustermap()
+        self.generate_volcano_plot()
+
+# Example usage
+count_table_file = sys.argv[1]
+metadata_file = sys.argv[2]
+genes_colname = str(sys.argv[3])
+control_condition = str(sys.argv[4])
+unnormal_condition = str(sys.argv[5])
+padj_value = float(sys.argv[6])
+log2FoldChange_value = float(sys.argv[7])
+
+deseq_analyzer = DeseqAnalyzer(
+    count_table_file, metadata_file,
+    genes_colname=genes_colname,
+    control_condition=control_condition,
+    unnormal_condition=unnormal_condition,
+    padj_value=padj_value,
+    log2FoldChange_value=log2FoldChange_value
+)
+
+deseq_analyzer.run_analysis()
---
+++ src/gws_omix/rna_seq/pydesq2_count.py
@@ -0,0 +1,117 @@
+import os
+from gws_core import (ConfigParams, ConfigSpecs, File, ShellProxy,
+                      FloatParam, InputSpec, InputSpecs, IntParam, OutputSpec, TableImporter,
+                      OutputSpecs, StrParam, Task, TaskFileDownloader, Table,
+                      TaskInputs, TaskOutputs, task_decorator , PlotlyResource , CondaShellProxy)
+
+import scanpy as sc
+import plotly.express as px
+import pandas as pd
+from gws_omix.base_env.pydesq2_env_task import Pydesq2ShellProxyHelper
+
+
+@task_decorator("Pydesq2", human_name="Differential analysis",
+                short_description="Differential analysis using pydesq2")
+
+class Pydesq2(Task):
+    """
+    xxxxxxxxxxxxxxxxxxxxxxxxxxxxx
+    xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx
+    xxxxxxxxxxxxxxxxxxxxxxxxxxx
+    """
+
+    input_specs: InputSpecs = InputSpecs({
+        'count_table_file': InputSpec(File, human_name="count table matrix", short_description="count table matrix"),
+        'metadata_file': InputSpec(File, human_name="metadata file ", short_description="tsv metadata file"),
+
+    })
+
+    output_specs: OutputSpecs = OutputSpecs({
+        'table_1': OutputSpec(Table, human_name="differantial expression result", short_description="xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx"),
+        'plotly_result': OutputSpec(
+            PlotlyResource, human_name="pathway_pca",
+            short_description="Show the difference after dimensional reduction via principal component analysis."),
+        'file_1': OutputSpec(File, human_name="heatmap", short_description="xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx"),
+        'file_2': OutputSpec(File, human_name="volcano plot", short_description="xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx")
+    })
+
+    config_specs: ConfigSpecs = {
+        "genes_colname": StrParam(short_description="Column name containing gene ids in expression matrix "),
+        "control_condition": StrParam(short_description="normal_condition"),
+        "unnormal_condition": StrParam(short_description="unnormal_condition"),
+        "padj_value": FloatParam(default_value=0.05, min_value=0.05, short_description="padj_value"),
+        "log2FoldChange_value": FloatParam(default_value=0.5, min_value=0.5, short_description="FloatParam"),
+    }
+
+    python_file_path = os.path.join(
+        os.path.abspath(os.path.dirname(__file__)),
+        "_pydesq2_count.py"
+    )
+
+    def run(self, params: ConfigParams, inputs: TaskInputs) -> TaskOutputs:
+        """ Run the task """
+        # retrive the input table
+        count_table_file: File = inputs['count_table_file']
+        metadata_file: File = inputs['metadata_file']
+        genes_colname = params["genes_colname"]
+        control_condition = params["control_condition"]
+        unnormal_condition = params["unnormal_condition"]
+        padj_value = params["padj_value"]
+        log2FoldChange_value = params["log2FoldChange_value"]
+
+        # retrieve the factor param value
+        shell_proxy: ShellProxy = Pydesq2ShellProxyHelper.create_proxy(self.message_dispatcher)
+
+
+        # call python file
+        cmd = f"python3 {self.python_file_path} {count_table_file.path} {metadata_file.path} {genes_colname} {control_condition} {unnormal_condition} {padj_value} {log2FoldChange_value}"
+        result = shell_proxy.run(cmd, shell_mode=True)
+
+        if result != 0:
+            raise Exception("An error occured during the execution of the script.")
+
+        heatmap_file_name = os.path.join(shell_proxy.working_dir, "Heatmap.png")
+        volcanoplot_file_name = os.path.join(shell_proxy.working_dir, "volcano_plot.png")
+        pydesq2_results_file_name = os.path.join(
+            shell_proxy.working_dir, "pydesq2_results_table.csv")
+
+        pydesq2_results_table = TableImporter.call(
+            File(pydesq2_results_file_name),
+            {'delimiter': ',', 'header': 0, 'file_format': 'csv', 'index_column': 0})
+
+
+        # PCA
+        pca_proportion_file_path = os.path.join(shell_proxy.working_dir, "pca_proportions.csv")
+        pca_file_path = os.path.join(shell_proxy.working_dir, "pca_metadata.csv")
+        plolty_resource = self.build_plotly(pca_file_path, pca_proportion_file_path)
+
+        # return the output table
+        return {
+            'file_1': File(heatmap_file_name),
+            'file_2': File(volcanoplot_file_name),
+            'plotly_result': plolty_resource,
+            'table_1': pydesq2_results_table
+
+        }
+
+    def build_plotly(self, pca_file_path, pca_proportion_file_path) -> PlotlyResource:
+        # Read saved files
+        data = pd.read_csv(pca_file_path)
+        proportion_data = pd.read_csv(pca_proportion_file_path, index_col=False)
+
+        # Create the scatter plot
+        fig = px.scatter(
+            data,
+            x='PC1',
+            y='PC2',
+            color='Condition',
+            text='Sample',
+            size_max=10,
+            opacity=0.7,
+            title='PCA Plot',
+            labels={'PC1': f'PC1 ({proportion_data["PC1 Proportion"].iloc[0] * 100:.2f}%)', 'PC2': f'PC2 ({proportion_data["PC2 Proportion"].iloc[0] * 100:.2f}%)'},
+        )
+
+        # Show the plot
+        return PlotlyResource(fig)
+
