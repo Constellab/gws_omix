f4854da17ad935d0c27e8ab89dff131d832b0f3d
Benjamin Maisonneuve <bmaisonneuve@gencovery.com>
2024-06-14 10:12:51 +0000

    (feat) remove salmon_index and clean tests

--- src/gws_omix/__init__.py
+++ src/gws_omix/__init__.py
@@ -1,11 +1,1 @@
-# Blast_EC
-# Merge EC List
-from .aligment.ec_list_merger import ECListMerger
-# data filtering
-from .data_filtering.trim_galore import TrimGalore
 from .file.fastq_folder import FastqFolder
-from .rna_seq.deseq2_differential_expression import DESeq2DifferentialAnalysis
-from .rna_seq.htseq_count import HTSeqCount
-from .rna_seq.salmon_index import SalmonIndex
-# Merge EC List
-from .rna_seq.salmon_quant_mapping import SalmonQuantMapping
